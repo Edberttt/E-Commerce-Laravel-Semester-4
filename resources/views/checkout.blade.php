e9a289c3e9e1670427c1d74fa470a14377a1da6a
EdbertCh27 <echandradinata@student.ciputra.ac.id>
2023-05-30 11:48:16 +0700

    perbaikin css

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -30,28 +30,47 @@
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <!--===============================================================================================-->
 
+	<link rel="icon" class="favIcon" href="{{ asset('../images/favicon (3).ico') }}">
 </head>
 <body class="animsition">
 	
 	<!-- Header -->
-	<header>
+	<header class="header-v4">
 		<!-- Header desktop -->
 		<div class="container-menu-desktop">
 			<!-- Topbar -->
 			<div class="top-bar">
 				<div class="content-topbar flex-sb-m h-full container">
 					<div class="left-top-bar">
-						Free shipping for standard order over IDR 1.000.000
+						Free shipping for standard order over $100
+					</div>
+
+					<div class="right-top-bar flex-w h-full">
+						<a href="#" class="flex-c-m trans-04 p-lr-25">
+							Help & FAQs
+						</a>
+
+						<a href="#" class="flex-c-m trans-04 p-lr-25">
+							My Account
+						</a>
+
+						<a href="#" class="flex-c-m trans-04 p-lr-25">
+							EN
+						</a>
+
+						<a href="#" class="flex-c-m trans-04 p-lr-25">
+							USD
+						</a>
 					</div>
 				</div>
 			</div>
 
-			<div class="wrap-menu-desktop">
+			<div class="wrap-menu-desktop how-shadow1">
 				<nav class="limiter-menu-desktop container">
 					
 					<!-- Logo desktop -->		
-					<a href="{{ url('/')}}/" class="logo">
-						<img src="images/icons/logo.png" alt="IMG-LOGO">
+					<a href="#" class="logo">
+					<img src="images/icons/logo.png" alt="IMG-LOGO">
 					</a>
 
 					<!-- Menu desktop -->
@@ -59,18 +78,23 @@
 						<ul class="main-menu">
 							<li class="active-menu">
 								<a href="{{ url('/')}}/">Home</a>
+								<!-- <ul class="sub-menu">
+									<li><a href="{{ url('/')}}/">Homepage 1</a></li>
+									<li><a href="home-02.html">Homepage 2</a></li>
+									<li><a href="home-03.html">Homepage 3</a></li>
+								</ul> -->
 							</li>
 
 							<li>
-								<a href="{{ url('/') }}/product">Shop</a>
+								<a href="{{ url('/')}}product">Shop</a>
 							</li>
 
 							<li class="label1" data-label1="hot">
-								<a href="{{ url('/') }}/features_filter">Features</a>
+								<a href="{{ url('/')}}/features_filter">Features</a>
 							</li>
 
 							<li>
-								<a href="{{ url('/') }}/contact">Contact</a>
+								<a href="{{ url('/')}}contact">Contact</a>
 							</li>
 						</ul>
 					</div>	
@@ -85,13 +109,14 @@
 							<i class="zmdi zmdi-shopping-cart"></i>
 						</div>
 
-						<a href="{{ url('/') }}/wishlist-detail" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti" data-notify="2">
+						<a href="wishlist-detail.html" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti" data-notify="2">
 							<i class="zmdi zmdi-favorite-outline"></i>
 						</a>
 
-						<a href="{{ url('/') }}/account" class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-account">
+						<a href="account.html" class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-account">
 							<i class="zmdi zmdi-account"></i>
-						</a>		  
+						</a>
+						  					  						  
 					</div>
 				</nav>
 				
@@ -102,7 +127,7 @@
 		<div class="wrap-header-mobile">
 			<!-- Logo moblie -->		
 			<div class="logo-mobile">
-				<a href="{{ url('/') }}/"><img src="images/icons/logo.png" alt="IMG-LOGO"></a>
+				<a href="index.html"><img src="images/icons/logo.png" alt="IMG-LOGO"></a>
 			</div>
 
 			<!-- Icon header -->
@@ -111,15 +136,15 @@
 					<input type="text" placeholder="Search">
 				</form>
 				
-				<div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti js-show-cart" data-notify="3">
+				<div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti js-show-cart" data-notify="2">
 					<i class="zmdi zmdi-shopping-cart"></i>
 				</div>
 
-				<a href="{{ url('/') }}/wishlist-detail" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti" data-notify="2">
+				<a href="w" class="dis-block icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti" data-notify="0">
 					<i class="zmdi zmdi-favorite-outline"></i>
 				</a>
 
-				<a href="{{ url('/') }}/account" class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-account">
+				<a href="account.html" class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-account">
 					<i class="zmdi zmdi-account"></i>
 				</a>
 
@@ -139,26 +164,49 @@
 			<ul class="topbar-mobile">
 				<li>
 					<div class="left-top-bar">
-						Free shipping for standard order over IDR 1.000.000
+						Free shipping for standard order over $100
+					</div>
+				</li>
+
+				<li>
+					<div class="right-top-bar flex-w h-full">
+						<a href="#" class="flex-c-m p-lr-10 trans-04">
+							Help & FAQs
+						</a>
+
+						<a href="#" class="flex-c-m p-lr-10 trans-04">
+							My Account
+						</a>
+
+						<a href="#" class="flex-c-m p-lr-10 trans-04">
+							EN
+						</a>
+
+						<a href="#" class="flex-c-m p-lr-10 trans-04">
+							USD
+						</a>
 					</div>
 				</li>
 			</ul>
 
 			<ul class="main-menu-m">
 				<li>
-					<a href="{{ url('/') }}/">Home</a>
+					<a href="{{ url('/')}}/">Home</a>
+					<span class="arrow-main-menu-m">
+						<i class="fa fa-angle-right" aria-hidden="true"></i>
+					</span>
 				</li>
 
 				<li>
-					<a href="{{ url('/') }}/product">Shop</a>
+					<a href="{{ url('/')}}product">Shop</a>
 				</li>
 
 				<li>
-					<a href="{{ url('/') }}/features_filter" class="label1 rs1" data-label1="hot">Features</a>
+					<a href="{{ url('/')}}features_filter" class="label1 rs1" data-label1="hot">Features</a>
 				</li>
 
 				<li>
-					<a href="{{ url('/') }}/contact">Contact</a>
+					<a href="{{ url('/')}}contact">Contact</a>
 				</li>
 			</ul>
 		</div>
@@ -199,60 +247,48 @@
 				<ul class="header-cart-wrapitem w-full">
 					<li class="header-cart-item flex-w flex-t m-b-12">
 						<div class="header-cart-item-img">
-
 							<img src="images/kacamata/Converse_F_CO_MCV5076LB_001_52-removebg-preview.png" alt="IMG">
-
 						</div>
 
 						<div class="header-cart-item-txt p-t-8">
 							<a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04 js-show-modal1">
-
 								Converse F CO MCV5076LB 001 52
 							</a>
 
 							<span class="header-cart-item-info">
 								1 x Rp 1.680.000
-
 							</span>
 						</div>
 					</li>
 
 					<li class="header-cart-item flex-w flex-t m-b-12">
 						<div class="header-cart-item-img">
-
 							<img src="images/kacamata/Christian Dior F CD MYDIORO1 086 54.jpg" alt="IMG">
-
 						</div>
 
 						<div class="header-cart-item-txt p-t-8">
 							<a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
-
 								Christian Dior F CD MYDIORO1 086 54
 							</a>
 
 							<span class="header-cart-item-info">
 								1 x Rp 5.520.000
-
 							</span>
 						</div>
 					</li>
 
 					<li class="header-cart-item flex-w flex-t m-b-12">
 						<div class="header-cart-item-img">
-
 							<img src="images/kacamata/Tommy Hilfiger F TH 0090 J5G 52.png" alt="IMG">
-
 						</div>
 
 						<div class="header-cart-item-txt p-t-8">
 							<a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
-
 								Tommy Hilfiger F TH 0090 J5G 52
 							</a>
 
 							<span class="header-cart-item-info">
 								1 x Rp 2.100.000
-
 							</span>
 						</div>
 					</li>
@@ -260,17 +296,15 @@
 				
 				<div class="w-full">
 					<div class="header-cart-total w-full p-tb-40">
-
 						Total: Rp 9.300.000
-
 					</div>
 
 					<div class="header-cart-buttons flex-w w-full">
-						<a href="{{ url('/') }}/shoping-cart" class="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-r-8 m-b-10">
+						<a href="shoping-cart.html" class="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-r-8 m-b-10">
 							View Cart
 						</a>
 
-						<a href="{{ url('/') }}/shoping-cart" class="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-b-10">
+						<a href="{{ url('/')}}checkout" class="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-b-10">
 							Check Out
 						</a>
 					</div>
@@ -455,23 +489,67 @@
 										Calculate Shipping
 									</span>
 
-									<div class="rs1-select2 rs2-select2 bor8 bg0 m-b-12 m-t-9">
+									<!-- <div class="rs1-select2 rs2-select2 bor8 bg0 m-b-12 m-t-9">
 										<select class="js-select2" name="time">
 											<option disabled selected>Select Province...</option>
+											<option>Nanggroe Aceh Darussalam</option>
+											<option>Sumatera Utara</option>
+											<option>Sumatera Selatan</option>
+											<option>Sumatera Barat</option>
+											<option>Bengkulu</option>
+											<option>Riau</option>
+											<option>Kepulauan Riau</option>
+											<option>Jambi</option>
+											<option>Lampung</option>
+											<option>Bangka Belitung</option>
+											<option>Kalimantan Barat</option>
+											<option>Kalimantan Timur</option>
+											<option>Kalimantan Selatan</option>
+											<option>Kalimantan Tengah</option>
+											<option>Kalimantan Utara</option>
+											<option>Banten</option>
+											<option>DKI Jakarta</option>
 											<option>Jawa Timur</option>
 											<option>Jawa Tengah</option>
                                             <option>Jawa Barat</option>
                                             <option>Jakarta</option>
-                                            <option>Yogyakarta</option>
-                                            <option>Sulawesi Tengah</option>
+                                            <option>Daerah Istimewa Yogyakarta</option>
+											<option>Bali</option>
+											<option>Nusa Tenggara Barat</option>
+											<option>Nusa Tenggara Timur</option>
+											<option>Gorontalo</option>
+                                            <option>Sulawesi Utara</option>
+											<option>Sulawesi Barat</option>
+											<option>Sulawesi Tengah</option>
+											<option>Sulawesi Tenggara</option>
                                             <option>Sulawesi Selatan</option>
+											<option>Maluku Utara</option>
+											<option>Maluku</option>
+											<option>Papua Barat</option>
+											<option>Papua</option>
+											<option>Papua Tengah</option>
+											<option>Papua Pegunungan</option>
+											<option>Papua Selatan</option>
+											<option>Papua Barat Daya</option>
+										</select>
+										<div class="dropDownSelect2"></div>
+									</div> -->
+
+									<div class="rs1-select2 rs2-select2 bor8 bg0 m-b-12 m-t-9">
+										<select class="js-select2" name="City" id="City">
+											<option disabled selected>Select City...</option>
+											<option>Surabaya Barat</option>
+											<option>Surabaya Timur</option>
+											<option>Surabaya Utara</option>
+											<option>Surabaya Selatan</option>
+											<option>Surabaya Tengah</option>
 										</select>
 										<div class="dropDownSelect2"></div>
 									</div>
 
-									<div class="bor8 bg0 m-b-12">
-										<input class="stext-111 cl8 plh3 size-111 p-lr-15" type="text" name="state" placeholder="Kabupaten / Kota">
-									</div>
+									<!-- <div class="bor8 bg0 m-b-12">
+										<input class="stext-111 cl8 plh3 size-111 p-lr-15" type="text" name="state" placeholder="Kota">
+									</div> -->
 
 									<div class="bor8 bg0 m-b-22">
 										<input class="stext-111 cl8 plh3 size-111 p-lr-15" type="text" name="alamat" placeholder="Alamat">

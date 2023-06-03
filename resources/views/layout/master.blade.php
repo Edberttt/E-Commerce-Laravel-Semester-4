97d88811fe1e2d45fc41377b5a13ff6113a10260
Aqilla Shahbani Mahazoya <mahazoyaaa@outlook.com>
2023-06-03 14:40:07 +0700

    HAPUS SEARCH ATAS

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -82,9 +82,9 @@
 					<!-- Icon header -->
 					<div class="wrap-icon-header flex-w flex-r-m">
 						@if(session()->has('status'))	
-						<form>
+						<!-- <form>
 							<input type="text" placeholder="Search">
-						</form>
+						</form> -->
 						<div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart" data-notify="3">
 							<i class="zmdi zmdi-shopping-cart"></i>
 						</div>
@@ -112,9 +112,9 @@
 
 			<!-- Icon header -->
 			<div class="wrap-icon-header flex-w flex-r-m m-r-15">
-				<form>
+				<!-- <form>
 					<input type="text" placeholder="Search">
-				</form>
+				</form> -->
 				
 				<div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti js-show-cart" data-notify="3">
 					<i class="zmdi zmdi-shopping-cart"></i>
